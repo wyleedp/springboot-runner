6ba01e2de445d4d05def25fa7fbf215c0364ffc3
wyleedp <wyleedp@gmail.com>
2022-04-19 16:53:28 +0900

    최초 커밋

---
+++ src/main/java/kr/co/wyleedp/springboot/runner/SpringbootRunner1Application.java
@@ -0,0 +1,13 @@
+package kr.co.wyleedp.springboot.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRunner1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRunner1Application.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/wyleedp/springboot/runner/SpringbootRunner1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wyleedp.springboot.runner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRunner1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
